4301457292dfc35dadf429f0e5fbbd070e47fe84
kaylindevchand <kaydevchand@gmail.com>
2020-05-28 15:12:23 -0400

    Create react project

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import PrototypeComponent from './prototype/PrototypeComponent';
+
+function App() {
+  return (
+    <div className="container">
+      <PrototypeComponent/>
+    </div>
+  );
+}
+
+export default App;
