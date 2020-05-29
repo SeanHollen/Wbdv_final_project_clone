fed4f9e1b1de71bcbafd5f2190b4bf6e9802e9d8
sean.hollen <hollen.s@husky.neu.edu>
2020-05-29 16:19:13 -0400

    some work on PrototypeComponent. The files I added shouldnt matter

--- src/prototype/PrototypeComponent.js
+++ src/prototype/PrototypeComponent.js
@@ -1,9 +1,32 @@
 import React from "react";
 
-const PrototypeComponent = () => {
-    return(
-      <h2>English to French Translator</h2>
+class PrototypeComponent extends React.Component {
+  state = {
+    search: "",
+  }
+  render() {
+    return (
+      <div className="container">
+        <div className="center">
+          <h2>English to French Translator</h2>
+          <input className="" type="text"
+            onChange={(event) => this.setState({
+              search: event.target.value
+            })}></input>
+          <button 
+          onClick={
+            () => this.query(this.state.search)}>
+            Add Course
+        </button>
+        </div>
+      </div>
     )
   }
-  
-  export default PrototypeComponent
+  query(search) {
+    console.log(search); 
+  }
+}
+
+
+
+export default PrototypeComponent
