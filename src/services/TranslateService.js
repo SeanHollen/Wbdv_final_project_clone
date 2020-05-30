cb68b15b5d64fbfc8fd978aa99d0d7cd90851e06
kaylindevchand <kaydevchand@gmail.com>
2020-05-30 13:22:24 -0400

    Change to api mymemory

--- src/services/TranslateService.js
+++ src/services/TranslateService.js
@@ -1,30 +1,9 @@
-// API Example: https://cloud.ibm.com/apidocs/language-translator?code=node#translate
 
-const LanguageTranslatorV3 = require('ibm-watson/language-translator/v3');
-const { IamAuthenticator } = require('ibm-watson/auth');
+const translateFrench = (search) => {
+  return fetch('https://api.mymemory.translated.net/get?q=' + search + '&langpair=en|fr')
+    .then((response) => response.json())    
+}
 
-const languageTranslator = new LanguageTranslatorV3({
-  version: '2018-05-01',
-  authenticator: new IamAuthenticator({
-    apikey: 'nEqUpV4HFIX7Sy-enSxWqIUriXtDJAHeK2TFt8CL4-6O',
-  }),
-  url: 'https://api.us-south.language-translator.watson.cloud.ibm.com/instances/78e6c599-229d-42d5-98ca-5449261008fe/v3/translate?version=2018-05-01',
-});
-
-const translateParams = {
-  text: 'Hello',
-  modelId: 'en-fr',
-};
-
-languageTranslator.translate(translateParams)
-  .then(translationResult => {
-    console.log(JSON.stringify(translationResult, null, 2));
-  })
-  .catch(err => {
-    console.log('error:', err);
-  });
-
-  export default {
-    languageTranslator
-  }
-  
+export default {
+  translateFrench
+}
