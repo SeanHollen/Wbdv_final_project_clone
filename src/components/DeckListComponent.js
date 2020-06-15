312cf7391cd2832d9d3c2cb1d66a7ad048e12c14
kaylindevchand <kaydevchand@gmail.com>
2020-06-15 11:05:39 -0400

    Deck list displays decks

--- src/components/DeckListComponent.js
+++ src/components/DeckListComponent.js
@@ -1,9 +1,42 @@
 import React from "react";
+import DeckService from "../services/DeckService";
+import {Link} from "react-router-dom";
 
 class DeckListComponent extends React.Component {
 
+	state = {
+		decks: []
+	}
+
+	componentDidMount() {
+		DeckService.findAllDecks().then(
+			decks => {
+				this.setState({
+					decks: decks
+				})
+				console.log(this.state.decks)
+			}
+		)
+	}
+
 	render() {
-		return <h4>Deck List</h4>
+		return (
+			<div>
+				<h4>Decks {this.state.decks.length}</h4>
+				<ul className="list-group">
+					{
+						this.state.decks.map(deck =>
+						<li key={deck.id} className="list-group-item">
+							<Link to={`/decks/${deck.id}/flashcards`}>
+								{deck.name}
+							</Link>
+						</li>)
+					}
+
+				</ul>
+			</div>
+
+		)
 	}
 
 }
