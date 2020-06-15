d6545e70f08dd810704626e2efc3bcc1c7ed7ccb
kaylindevchand <kaydevchand@gmail.com>
2020-06-14 22:17:46 -0400

    Deck service

---
+++ src/services/DeckService.js
@@ -0,0 +1,39 @@
+const findAllDecks = () => {
+  return fetch("http://localhost:8080/api/decks")
+    .then(response => response.json())
+}
+
+const deleteDeck = (deckId) => {
+  return fetch("http://localhost:8080/api/decks/" + deckId, {
+    method: 'DELETE'
+  })
+    .then(response => response.json())
+}
+
+const updateDeck = (deckId, newDeck) =>
+  fetch(`http://localhost:8080/api/decks/${deckId}`, {
+    method: 'PUT',
+    body: JSON.stringify(newDeck),
+    headers: {
+      'content-type': 'application/json'
+    }
+  })
+    .then(response => response.json())
+
+const createDeck = (deck) =>
+  fetch(`http://localhost:8080/api/decks`, {
+    method: 'POST',
+    body: JSON.stringify(deck),
+    headers: {
+      'content-type': 'application/json'
+    }
+  })
+    .then(response => response.json())
+
+
+export default {
+  findAllDecks,
+  deleteDeck,
+  createDeck,
+  updateDeck,
+}
