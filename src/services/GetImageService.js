fa22b694cebb27aadcbe1d2220425a2c46924446
sean.hollen <hollen.s@husky.neu.edu>
2020-06-17 17:59:58 -0400

    images api

--- src/services/GetImageService.js
+++ src/services/GetImageService.js
@@ -2,7 +2,7 @@
 const GetImageService = (search) => {
   let cx = '010392335913420737404:y05xze-mcki';
   let key = 'AIzaSyD4p5fQdFLbrk46k0VstKHhWmQ5MmktLG0'
-  return fetch('https://www.googleapis.com/customsearch/v1?key=' + key + '&cx=' + cx + '&searchType=image&q=search')
+  return fetch('https://www.googleapis.com/customsearch/v1?key=' + key + '&cx=' + cx + '&q=' + search + '&searchType=image')
     .then((response) => response.json())
 }
 
