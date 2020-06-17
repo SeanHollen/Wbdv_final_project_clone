c4b871934b310593f868f8d7771983a1be953ff5
sean.hollen <hollen.s@husky.neu.edu>
2020-06-17 08:04:04 -0400

    trying to search images (doesnt really work)

---
+++ src/services/GetImageService.js
@@ -0,0 +1,11 @@
+
+const GetImageService = (search) => {
+  let cx = '010392335913420737404:y05xze-mcki';
+  let key = 'AIzaSyD4p5fQdFLbrk46k0VstKHhWmQ5MmktLG0'
+  return fetch('https://www.googleapis.com/customsearch/v1?key=' + key + '&cx=' + cx + '&searchType=image&q=search')
+    .then((response) => response.json())
+}
+
+export default {
+  getImageService
+}
