076e3fb776f623143e8278ad6fcfe96f6684c766
kaylindevchand <kaydevchand@gmail.com>
2020-06-18 00:00:53 -0400

    Add My Decks to home if logged in

--- src/components/HomeComponent.js
+++ src/components/HomeComponent.js
@@ -1,19 +1,50 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import {Link} from "react-router-dom";
+import UserService from "../services/UserService";
 
-const HomeComponent = () =>
-  <div>
-    <div className="list-group">
-      <Link className="list-group-item" to='/decks'>
-        Public Decks
-      </Link>
-      <Link className="list-group-item" to='/profiles'>
-        All Users
-      </Link>
-      <Link className="list-group-item" to='/lesson/lesson1'>
-        Lesson 1: Colors
-      </Link>
-    </div>
-  </div>
+class HomeComponent extends React.Component {
+
+	state = {
+		user: ""
+	}
+
+	componentDidMount() {
+		UserService.profile().catch(e => {
+		})
+			.then(user => {
+				if (user)
+					this.setState({user: user.name})
+			})
+	}
+
+	render() {
+		return (
+			<div>
+				{
+					this.state.user &&
+						<h2>Welcome {this.state.user}</h2>
+				}
+				<div className="list-group">
+					<Link className="list-group-item" to='/decks'>
+						Public Decks
+					</Link>
+					{
+						this.state.user &&
+							<Link className="list-group-item" to='/mydecks'>
+								My Decks
+							</Link>
+					}
+					<Link className="list-group-item" to='/profiles'>
+						All Users
+					</Link>
+					<Link className="list-group-item" to='/lesson/lesson1'>
+						Lesson 1: Colors
+					</Link>
+				</div>
+			</div>
+		)
+	}
+}
+
 
 export default HomeComponent
