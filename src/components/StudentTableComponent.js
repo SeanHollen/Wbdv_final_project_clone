eb6408cc7d45488f0e516f5b7806650dd3a04e6c
kaylindevchand <kaydevchand@gmail.com>
2020-06-18 00:33:08 -0400

    Forms to edit and delte students

--- src/components/StudentTableComponent.js
+++ src/components/StudentTableComponent.js
@@ -8,7 +8,8 @@ class StudentTableComponent extends React.Component {
 		students: [],
 		newStudent: {
 			role: "STUDENT"
-		}
+		},
+		editingStudent: {}
 	}
 
 	componentDidMount() {
@@ -21,6 +22,15 @@ class StudentTableComponent extends React.Component {
 		UserService.register(this.state.newStudent)
 		window.location.reload();
 		return false;
+	}
+
+	updateStudent() {
+		UserService.updateProfile(this.state.editingStudent)
+		window.location.reload()
+	}
+
+	deleteStudent() {
+
 	}
 
 	render() {
@@ -41,20 +51,129 @@ class StudentTableComponent extends React.Component {
 					<tbody>
 					{
 						this.state.students.map(student =>
-							<tr key={student.id}>
-								<td>{student.username}</td>
-								<td>***</td>
-								<td>{student.email}</td>
-								<td>{student.phone}</td>
-								<td>{student.dob}</td>
+
+							<tr>
 								<td>
-									{/*<Link to={`/profile/${student.id}`} className="btn btn-primary text-white">Edit</Link>*/}
+									{
+										this.state.editingStudent.id !== student.id &&
+										student.username
+									}
+									{
+										this.state.editingStudent.id === student.id &&
+										<input className="form-control" value={this.state.editingStudent.username} placeholder="Username"
+													 onChange={(e) => {
+														 const newUsername = e.target.value
+														 this.setState(prevState => ({
+															 editingStudent: {
+																 ...prevState.editingStudent,
+																 username: newUsername
+															 }
+														 }))
+													 }}/>
+									}
+								</td>
+								<td>
+									{
+										this.state.editingStudent.id !== student.id &&
+										<a>***</a>
+									}
+									{
+										this.state.editingStudent.id === student.id &&
+										<input className="form-control" type="password" value={this.state.editingStudent.password}
+													 placeholder="Password"
+													 onChange={(e) => {
+														 const newPassword = e.target.value
+														 this.setState(prevState => ({
+															 editingStudent: {
+																 ...prevState.editingStudent,
+																 password: newPassword
+															 }
+														 }))
+													 }}/>
+									}
+								</td>
+								<td>
+									{
+										this.state.editingStudent.id !== student.id &&
+										student.email
+									}
+									{
+										this.state.editingStudent.id === student.id &&
+										<input className="form-control" type="email" value={this.state.editingStudent.email}
+													 placeholder="Email"
+													 onChange={(e) => {
+														 const newEmail = e.target.value
+														 this.setState(prevState => ({
+															 editingStudent: {
+																 ...prevState.editingStudent,
+																 email: newEmail
+															 }
+														 }))
+													 }}/>
+									}
+								</td>
+								<td>
+									{
+										this.state.editingStudent.id !== student.id &&
+										student.phone
+									}
+									{
+										this.state.editingStudent.id === student.id &&
+										<input className="form-control" value={this.state.editingStudent.phone}
+													 placeholder="Phone Number"
+													 onChange={(e) => {
+														 const newPhone = e.target.value
+														 this.setState(prevState => ({
+															 editingStudent: {
+																 ...prevState.editingStudent,
+																 phone: newPhone
+															 }
+														 }))
+													 }}/>
+									}
+								</td>
+								<td>
+									{
+										this.state.editingStudent.id !== student.id &&
+										student.dob
+									}
+									{
+										this.state.editingStudent.id === student.id &&
+										<input className="form-control" type="date" value={this.state.editingStudent.dob}
+													 onChange={(e) => {
+														 const newDob = e.target.value
+														 this.setState(prevState => ({
+															 newStudent: {
+																 ...prevState.editingStudent,
+																 dob: newDob
+															 }
+														 }))
+													 }}/>
+									}
+								</td>
+								<td>
+									{
+										this.state.editingStudent.id !== student.id &&
+										<a className="btn btn-primary text-white"
+											 onClick={() => {
+												 this.setState({editingStudent: student})
+											 }}>Edit</a>
+									}
+									{
+										this.state.editingStudent.id === student.id &&
+										<span className="btn-group">
+											<a className="btn btn-success text-white"
+												 onClick={() => this.updateStudent()}>Update</a>
+											<a className="btn btn-danger text-white"
+												 onClick={() => this.deleteStudent()}>Delete</a>
+										</span>
+									}
 								</td>
 							</tr>
 						)}
 					<tr>
 						<td>
-							<input className="form-control" value={this.state.newStudent.username}
+							<input className="form-control" value={this.state.newStudent.username} placeholder="Username"
 										 onChange={(e) => {
 											 const newUsername = e.target.value
 											 this.setState(prevState => ({
@@ -67,6 +186,7 @@ class StudentTableComponent extends React.Component {
 						</td>
 						<td>
 							<input className="form-control" type="password" value={this.state.newStudent.password}
+										 placeholder="Password"
 										 onChange={(e) => {
 											 const newPassword = e.target.value
 											 this.setState(prevState => ({
@@ -79,6 +199,7 @@ class StudentTableComponent extends React.Component {
 						</td>
 						<td>
 							<input className="form-control" type="email" value={this.state.newStudent.email}
+										 placeholder="Email"
 										 onChange={(e) => {
 											 const newEmail = e.target.value
 											 this.setState(prevState => ({
@@ -91,6 +212,7 @@ class StudentTableComponent extends React.Component {
 						</td>
 						<td>
 							<input className="form-control" value={this.state.newStudent.phone}
+										 placeholder="Phone Number"
 										 onChange={(e) => {
 											 const newPhone = e.target.value
 											 this.setState(prevState => ({
