ba5560c74432d6d4b6035d761652a02ae53ba8dc
sean.hollen <hollen.s@husky.neu.edu>
2020-06-18 21:12:11 -0400

    following list

--- src/components/FollowingComponent.js
+++ src/components/FollowingComponent.js
@@ -4,7 +4,7 @@ import UserService from "../services/UserService";
 export default class FlashCardEditorComponent extends React.Component {
 
     state = {
-        user: "abc", 
+        user: "", 
 		following: []
     }
     
@@ -19,16 +19,31 @@ export default class FlashCardEditorComponent extends React.Component {
         UserService.following().then(res => {
             console.log("following"); 
             console.log(res); 
-            this.setState({user: res});
+            this.setState({following: res});
         })
     }
 
-    render() { return <div>
-        <h3>Hello {this.state.user}, you are following: </h3>
-        {
-            this.state.following.map(user =>
-                <div>{user}</div>)
+    render() { 
+        if (this.state.user == "") {
+            return <div></div>
         }
+        return <div>
+        <h3>Hello {this.state.user}, you are following: </h3>
+        <table style={{width:'100%'}}>
+        <tr>
+                <th>Name</th>
+                <th>Username</th>
+                <th>Role</th>
+                </tr>
+        {
+            this.state.following.map(followed =>
+                <tr>
+                <td>{followed.name}</td>
+                <td>{followed.username}</td>
+                <td>{followed.role}</td>
+                </tr>)
+        }
+        </table>
         </div>
     }
 }
